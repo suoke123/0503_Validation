159d1435e45854a70cffe3057e6f4c1b244c55b7
suoke123 <asak222@hanmail.net>
2021-05-03 17:24:23 +0900

    0503_Validation

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $("button").click(function(){
+
+        var blankChk = $("#blankChk").val();
+        if (blankChk.trim() == "") {
+            $("#blankChkArea>span").text("공백안됨, 필수!");
+
+            $("#blankChk").val("").focus();
+            // 동일선택자에 메서드를 여러게 사용하여 마치 체인처럼 연결되었다고 하여
+            // 메서드체인(chain)이라고 부름
+        } else {
+            $("#blankChkArea>span").text("");
+        }
+
+    });
+
+});
